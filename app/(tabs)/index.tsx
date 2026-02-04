59919c3f58d2b4d95d7006ef9d0c542b468f47ba
Shuvo Halder <shuvohalder499@gmail.com>
2026-02-04 21:39:03 +0600

    ceate the header title and the completed task show in the hear and also show the progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,26 @@
-import { api } from "@/convex/_generated/api";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
 import useTheme from "@/hook/useThemes";
-import { useMutation, useQuery } from "convex/react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
 
-  const todos = useQuery(api.todos.getTodos);
-  console.log("Todos:", todos);
-
-  const addTodo = useMutation(api.todos.addTodo);
-  const clearTodos = useMutation(api.todos.clearTodos);
-
+  const homeStyles = createHomeStyles(colors);
   return (
-    <View style={Styles.container}>
-      <Text style={Styles.text}>Create New Todo App</Text>
-
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle Theme</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => addTodo({ text: "New Todo Item Added" })}
-      >
-        <Text>Add New Todo</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => clearTodos()}>
-        <Text>Clear All Todos</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle Theme</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-
-const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "olive",
-  },
-  text: {
-    color: "white",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-});
