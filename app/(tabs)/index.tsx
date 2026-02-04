4db1b636f70270003eecec7881ad1076eaf86532
Shuvo Halder <shuvohalder499@gmail.com>
2026-02-04 19:34:03 +0600

    crate the add todo and clear all todo function

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,33 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hook/useThemes";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  console.log("Todos:", todos);
+
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearTodos = useMutation(api.todos.clearTodos);
+
   return (
     <View style={Styles.container}>
       <Text style={Styles.text}>Create New Todo App</Text>
 
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle Theme</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => addTodo({ text: "New Todo Item Added" })}
+      >
+        <Text>Add New Todo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => clearTodos()}>
+        <Text>Clear All Todos</Text>
       </TouchableOpacity>
     </View>
   );
